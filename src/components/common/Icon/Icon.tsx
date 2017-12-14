8bd5465a55480af9789ce3a40e28b4982c1c53e4
sergioruizdavila <sergioruizdavila@gmail.com>
2017-12-14 10:12:20 -0500

    Mod: Cleaned App.scss in order to remove some import that don't work. Add: Created first version of 'SidebarWrapper' component Mod: Connected the SidebarWrapper component in Explore Page only to test

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -15,9 +15,11 @@ interface IconType {
     logo: () => JSX.Element;
     arrowDown: () => JSX.Element;
     chevronDown: () => JSX.Element;
+    chevronRight: () => JSX.Element;
     heart: () => JSX.Element;
     heartFull: () => JSX.Element;
     messageCircle: () => JSX.Element;
+    layer: () => JSX.Element;
     package: () => JSX.Element;
     eye: () => JSX.Element;
     search: () => JSX.Element;
@@ -127,6 +129,22 @@ class Icon extends React.Component<IconProps, {}> {
                     </svg>
                 );
             },
+            chevronRight: () => {
+                return (
+                    <svg className={iconClass}
+                         width={width}
+                         height={height}
+                         viewBox="0 0 24 24"
+                         fill="none"
+                         stroke={color}
+                         strokeWidth="2"
+                         strokeLinecap="round"
+                         strokeLinejoin="round"
+                         xmlns="http://www.w3.org/2000/svg">
+                         <polyline points="9 18 15 12 9 6" />
+                    </svg>
+                );
+            },
             heart: () => {
                 return (
                     <svg className={iconClass}
@@ -172,6 +190,24 @@ class Icon extends React.Component<IconProps, {}> {
                          strokeLinejoin="round"
                          xmlns="http://www.w3.org/2000/svg">
                          <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+                    </svg>
+                );
+            },
+            layer: () => {
+                return (
+                    <svg className={iconClass}
+                         width={width}
+                         height={height}
+                         viewBox="0 0 24 24"
+                         fill="none" 
+                         stroke={color}
+                         strokeWidth="2"
+                         strokeLinecap="round"
+                         strokeLinejoin="round"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <polygon points="12 2 2 7 12 12 22 7 12 2" />
+                        <polyline points="2 17 12 22 22 17" />
+                        <polyline points="2 12 12 17 22 12" />
                     </svg>
                 );
             },
---
+++ src/components/common/SidebarWrapper/SidebarWrapper.tsx
@@ -0,0 +1,168 @@
+/********************************/
+/*         DEPENDENCIES         */
+/********************************/
+import * as React from 'react';
+import { ChildProps } from 'react-apollo';
+
+import Icon from '../Icon/Icon';
+
+
+// -----------------------------------
+
+
+/********************************/
+/*      INTERFACES & TYPES      */
+/********************************/
+
+/* Own Props */
+type SidebarWrapperProps = {};
+
+/* Own States */
+type LocalStates = {
+    visible: boolean
+};
+
+/* Mapped State to Props */
+type StateProps = {};
+
+
+/***********************************************/
+/*              CLASS DEFINITION               */
+/***********************************************/
+class SidebarWrapper
+extends React.Component<ChildProps<SidebarWrapperProps & StateProps, {}>, LocalStates> {
+    
+    
+    /********************************/
+    /*         CONSTRUCTOR          */
+    /********************************/
+    constructor(props: ChildProps<SidebarWrapperProps & StateProps, {}>) {
+        super(props);
+
+        // Init state
+        this.state = {
+            visible: true
+        };
+
+        // Bind methods
+        this._handleToggleClick = this._handleToggleClick.bind(this);
+    }
+
+
+    /********************************/
+    /*       PRIVATE METHODS        */
+    /********************************/
+
+    /**
+     * @desc Handle Toggle click
+     * @method _handleToggleClick
+     * @example this._handleToggleClick()
+     * @private
+     * @param {any} e - Event
+     * @returns {void}
+     */
+    private _handleToggleClick(e: any) {
+        // Update the state
+        this._toggleVisibility();
+    }
+
+
+    /**
+     * @desc Toggle visibility
+     * @method _toggleVisibility
+     * @example this._toggleVisibility()
+     * @private
+     * @returns {void}
+     */
+    private _toggleVisibility() {
+        // Update the state
+        this.setState({ 
+            visible: !this.state.visible
+        });
+    }
+
+    
+    /********************************/
+    /*        RENDER MARKUP         */
+    /********************************/
+    render() {
+            
+        
+        /*         MARKUP          */
+        /***************************/
+        return (
+            <div className="SidebarWrapper">
+                <div className="Sidebar">
+                    <div className="Sidebar__header px-3 mt-3">
+                        {/* Logo */}
+                        <a className="sp-logo sp-logo--sm sp-logo--white m-0 link-reset" href="/">
+                            <Icon icon="logo" 
+                                iconClass="mr-2"
+                                width="20" height="20"/>
+                            <span>Stylepill</span>
+                        </a>
+                    </div>
+
+                    <div className="divider m-3" />
+
+                    <div className="Sidebar__content">
+                        <div className="title px-3 py-2">
+                            Components (100)
+                        </div>
+                        <div className="option px-3 py-1">
+                            <Icon icon="layer"
+                                iconClass="stroke-white strokeWidth-2 ml-2 mr-3"
+                                width="14" height="14"/>
+                            <span className="fontSize-sm fontWeight-6 color-white">
+                                All
+                            </span>
+                        </div>
+
+                        <div className="divider m-3 mt-4" />
+
+                        <div className="title px-3 py-2">
+                            Projects (2)
+                        </div>
+                        <div className="option px-3 py-1">
+                            <Icon icon="chevronRight"
+                                iconClass="stroke-white strokeWidth-3 ml-2 mr-1"
+                                width="16" height="16"/>
+                            <span className="fontSize-sm fontWeight-6 color-white">
+                                Stylepill
+                            </span>
+                        </div>
+                        <div className="option px-3 py-1">
+                            <Icon icon="chevronRight"
+                                iconClass="stroke-white strokeWidth-3 ml-2 mr-1"
+                                width="16" height="16"/>
+                            <span className="fontSize-sm fontWeight-6 color-white">
+                                Waysily
+                            </span>
+                        </div>
+                        <div className="option px-3 py-1">
+                            <Icon icon="chevronRight"
+                                iconClass="stroke-white strokeWidth-3 ml-2 mr-1"
+                                width="16" height="16"/>
+                            <span className="fontSize-sm fontWeight-6 color-white">
+                                Steroidesign
+                            </span>
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div className="MainContainer">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+
+    }
+
+}
+
+
+/*         EXPORT          */
+/***************************/
+export default SidebarWrapper;
