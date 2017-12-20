6b50ae39d0fab9ab418f182bd5d3cf0e3647db8c
sergioruizdavila <sergioruizdavila@gmail.com>
2017-12-20 15:19:57 -0500

    Mod: Refactor ComponentDetails, ComponentNew, ProjectDetails, ProjectList, ProjectNew on Dashboard in order to add a 'working' friendly message

--- src/components/pages/ProjectsPage/list/ProjectList.container.tsx
+++ src/components/pages/ProjectsPage/list/ProjectList.container.tsx
@@ -45,7 +45,15 @@ extends React.Component<ChildProps<ProjectListProps & StateProps, {}>, LocalStat
         /***************************/
         return (
             <div className="ProjectList">
-                Hello list
+                <ul className="sp-messageBlock m-0 mx-4 mt-4">
+                    <li className="sp-messageBlock__container sp-messageBlock__container--md">
+                        <div className="icon icon--md icon--working mt-4 mb-3" />
+                        <div className="text text--sm fontFamily-openSans fontWeight-7 color-extraDarkSmoke mb-4">
+                            <div>Coming soon!!</div>
+                            <div>We're working hard to finish this as soon as possible.</div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         );
 
