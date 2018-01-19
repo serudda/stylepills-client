06cd326d4a0310932b8c511ac5c4fdd841d19e8f
sergioruizdavila <sergioruizdavila@gmail.com>
2018-01-18 22:35:16 -0500

    Add: Added STAGING constants

--- src/core/constants/app.constants.tsx
+++ src/core/constants/app.constants.tsx
@@ -9,34 +9,39 @@ import { RgbaColor as RgbaColorModel } from './../../models/rgbaColor/rgbaColor.
 export const DEBUG = !!process.env.REACT_APP_LOCAL_ENV;
 
 /* Environments */
-export const LOCAL = 'local';
-export const DEV = 'development';
-export const PRD = 'production';
+export const LOCAL      = 'local';
+export const DEV        = 'development';
+export const STAGING    = 'staging';
+export const PRD        = 'production';
 
 /* Base Endpoints */
-export const DATA = '/graphql';
-export const AUTH_GOOGLE = '/auth/google';
-export const AUTH_LOGOUT = '/auth/logout';
+export const DATA           = '/graphql';
+export const AUTH_GOOGLE    = '/auth/google';
+export const AUTH_LOGOUT    = '/auth/logout';
 
 /* Server Urls */
-export const LOCAL_SERVER_URL = 'http://localhost:4000';
-export const DEV_SERVER_URL = 'https://stylepills-server-dev.herokuapp.com';
-export const PRD_SERVER_URL = 'https://stylepills-server.herokuapp.com';
+export const LOCAL_SERVER_URL   = 'http://localhost:4000';
+export const DEV_SERVER_URL     = 'https://stylepills-server-dev.herokuapp.com';
+export const STAGING_SERVER_URL = 'https://stylepill-server-staging.herokuapp.com';
+export const PRD_SERVER_URL     = 'https://stylepills-server.herokuapp.com';
 
 /* Data Base Endpoints */
-export const LOCAL_DATA_URL = `${LOCAL_SERVER_URL}${DATA}`;
-export const DEV_DATA_URL = `${DEV_SERVER_URL}${DATA}`;
-export const PRD_DATA_URL = `${PRD_SERVER_URL}${DATA}`;
+export const LOCAL_DATA_URL     = `${LOCAL_SERVER_URL}${DATA}`;
+export const DEV_DATA_URL       = `${DEV_SERVER_URL}${DATA}`;
+export const STAGING_DATA_URL   = `${STAGING_SERVER_URL}${DATA}`;
+export const PRD_DATA_URL       = `${PRD_SERVER_URL}${DATA}`;
 
 /* Google Auth Base Endpoints */
-export const LOCAL_AUTH_GOOGLE_URL = `${LOCAL_SERVER_URL}${AUTH_GOOGLE}`;
-export const DEV_AUTH_GOOGLE_URL = `${DEV_SERVER_URL}${AUTH_GOOGLE}`;
-export const PRD_AUTH_GOOGLE_URL = `${PRD_SERVER_URL}${AUTH_GOOGLE}`;
+export const LOCAL_AUTH_GOOGLE_URL      = `${LOCAL_SERVER_URL}${AUTH_GOOGLE}`;
+export const DEV_AUTH_GOOGLE_URL        = `${DEV_SERVER_URL}${AUTH_GOOGLE}`;
+export const STAGING_AUTH_GOOGLE_URL    = `${STAGING_SERVER_URL}${AUTH_GOOGLE}`;
+export const PRD_AUTH_GOOGLE_URL        = `${PRD_SERVER_URL}${AUTH_GOOGLE}`;
 
 /* Auth Logout Base Endpoints */
-export const LOCAL_AUTH_LOGOUT_URL = `${LOCAL_SERVER_URL}${AUTH_LOGOUT}`;
-export const DEV_AUTH_LOGOUT_URL = `${DEV_SERVER_URL}${AUTH_LOGOUT}`;
-export const PRD_AUTH_LOGOUT_URL = `${PRD_SERVER_URL}${AUTH_LOGOUT}`;
+export const LOCAL_AUTH_LOGOUT_URL      = `${LOCAL_SERVER_URL}${AUTH_LOGOUT}`;
+export const DEV_AUTH_LOGOUT_URL        = `${DEV_SERVER_URL}${AUTH_LOGOUT}`;
+export const STAGING_AUTH_LOGOUT_URL    = `${STAGING_SERVER_URL}${AUTH_LOGOUT}`;
+export const PRD_AUTH_LOGOUT_URL        = `${PRD_SERVER_URL}${AUTH_LOGOUT}`;
 
 /* Search params */
 export const ATOM_SEARCH_LIMIT = 9;
@@ -47,8 +52,8 @@ export type ATOM_SEARCH_TYPE_DEFAULT = typeof ATOM_SEARCH_TYPE_DEFAULT;
 export const ATOM_SEARCH_ORDER = 'DESC';
 
 /* Modal Types */
-export const ATOM_DETAILS_MODAL_TYPE = 'AtomDetailsModal';
-export const DUPLICATE_MODAL_TYPE = 'DuplicateModal';
+export const ATOM_DETAILS_MODAL_TYPE    = 'AtomDetailsModal';
+export const DUPLICATE_MODAL_TYPE       = 'DuplicateModal';
 
 /* Source Code Tabs (Atom Details) */
 export const ATOM_DETAILS_DEFAULT_OPTION_TAB = 'html';
