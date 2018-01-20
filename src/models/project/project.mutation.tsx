8f0ade1904a9282223b8809f8833c3bcf9b99016
sergioruizdavila <sergioruizdavila@gmail.com>
2018-01-20 16:32:04 -0500

    Mod: Enhanced create project mutation example

--- src/models/project/project.mutation.tsx
+++ src/models/project/project.mutation.tsx
@@ -46,6 +46,7 @@ export type CreateProjectInput = {
 CREATE_PROJECT_MUTATION
 mutation createProject($input: CreateProjectInput!) {
     createProject(input: $input){
+        id
         ok
         message
     }
